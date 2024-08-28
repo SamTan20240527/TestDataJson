3e5bf317041e7bcb7b0a2345224a48ea87f72624
Sam Tan <samtancm@gmail.com>
2024-08-28 15:55:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+﻿const options =  {
+   chart: {
+       type: 'bar',
+       height:"100%"
+   },
+   series:[
+      
+   ],
+   noData: {
+       text: "Loading..."
+   }
+ 
+  
+}
+
+
+
+
+// create the chart
+const chart = new ApexCharts(document.querySelector('#chart'), options);
+
+
+
+
+// render the chart
+chart.render()
+
+
+async function loadData() {
+   const response = await axios.get('data.json');
+   return response.data.yearly;
+}
+
+
+
+
+window.addEventListener('DOMContentLoaded', async ()=>{
+   let series = await loadData();
+   chart.updateSeries([{
+       'name': 'Sales',
+       'data': series
+   }])
+})
